fd5621f108d3970a414f8cc7496293a4b4414793
Iamnotagenius <kazakk9@gmail.com>
2024-02-17 01:50:38 +0300

    final refinements

--- src/first/main.cpp
+++ src/first/main.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <cctype>
-#include <cstdlib>
 #include <functional>
 #include <iostream>
 #include <mutex>
@@ -14,26 +13,34 @@
 #ifdef DEBUG
 #include "../debug/log.hpp"
 #endif
-#include "connection.hpp"
+#include "sum_sender.hpp"
 
 
 void thread1(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer);
-void thread2(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer);
+void thread2(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer, sum_sender& con);
 std::string replace_even(const std::string& digits);
 
 int main() {
-    std::string buffer;
-    std::mutex new_data;
-    std::mutex data_handled;
+    try {
+        std::string buffer;
+        std::mutex new_data;
+        std::mutex data_handled;
 
-    buffer.reserve(128);
-    new_data.lock();
+        sum_sender con(6666);
 
-    std::thread t1(thread1, std::ref(new_data), std::ref(data_handled), std::ref(buffer));
-    std::thread t2(thread2, std::ref(new_data), std::ref(data_handled), std::ref(buffer));
+        buffer.reserve(128);
+        new_data.lock();
 
-    t1.join();
-    t2.join();
+        std::thread t1(thread1, std::ref(new_data), std::ref(data_handled), std::ref(buffer));
+        std::thread t2(thread2, std::ref(new_data), std::ref(data_handled), std::ref(buffer), std::ref(con));
+        std::thread conn_thread([&]() { con.handle_connections(); });
+
+        t1.join();
+        t2.join();
+        conn_thread.detach();
+    } catch (const connection_exception& e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+    }
 }
 
 void thread1(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer) {
@@ -41,6 +48,9 @@ void thread1(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string&
     while (!std::cin.eof()) {
         std::cout << "$ ";
         std::cin >> input;
+        if (std::cin.eof()) {
+            break;
+        }
         if (input.length() > 64 || std::any_of(input.cbegin(), input.cend(), std::not_fn(isdigit))) {
             std::cerr << "Message is invalid" << std::endl;
             continue;
@@ -54,37 +64,40 @@ void thread1(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string&
         buffer = input;
         new_data_lock.unlock();
     }
+    new_data_lock.unlock();
+#ifdef DEBUG
+            debug::debug_log("Reading thread") << "Terminating...\n";
+#endif
 }
 
-void thread2(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer) {
+void thread2(std::mutex& new_data_lock, std::mutex& data_read_lock, std::string& buffer, sum_sender& con) {
     std::string input;
-    try {
-        connection con(6666);
-        std::thread conn_thread([&]() { con.handle_connections(); });
-        while (!std::cin.eof()) {
+    while (!std::cin.eof()) {
 #ifdef DEBUG
-            debug::debug_log("Writing thread") << "Waiting for data...\n";
+        debug::debug_log("Writing thread") << "Waiting for data...\n";
 #endif
-            new_data_lock.lock();
-            input = buffer;
-            buffer.clear();
-            data_read_lock.unlock();
+        new_data_lock.lock();
+        input = buffer;
+        buffer.clear();
+        data_read_lock.unlock();
 
-            std::cout << input << std::endl;
-
-            int sum = std::accumulate(input.cbegin(), input.cend(), 0, [](int lhs, char rhs) {
-                if (isdigit(rhs)) {
-                    return lhs + (rhs - '0');
-                }
-                return lhs;
-            });
-            con.send_sum(sum);
+        if (std::cin.eof()) {
+            break;
         }
-        conn_thread.join();
-    } catch (const connection_exception& e) {
-        std::cerr << "Caught exception: " << e.what() << std::endl;
-        exit(1);
+
+        std::cout << input << std::endl;
+
+        int sum = std::accumulate(input.cbegin(), input.cend(), 0, [](int lhs, char rhs) {
+            if (isdigit(rhs)) {
+                return lhs + (rhs - '0');
+            }
+            return lhs;
+        });
+        con.send_sum(sum);
     }
+#ifdef DEBUG
+            debug::debug_log("Writing thread") << "Terminating...\n";
+#endif
 }
 
 std::string replace_even(const std::string& digits) {
--- src/first/sum_sender.cpp
+++ src/first/sum_sender.cpp
@@ -23,9 +23,9 @@
 #ifdef DEBUG
 #include "../debug/log.hpp"
 #endif
-#include "connection.hpp"
+#include "sum_sender.hpp"
 
-connection::connection(int port, int max_connections) 
+sum_sender::sum_sender(int port, int max_connections) 
             : sockfd(socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)),
                 port(port),
                 queue_mutex(),
@@ -46,21 +46,23 @@ connection::connection(int port, int max_connections)
     listen(sockfd, max_connections);
 }
 
-void connection::handle_connections() {
-    while (true) {
+void sum_sender::handle_connections() {
+    while (!std::cin.eof() || !to_send.empty()) {
 #ifdef DEBUG
-        debug::debug_log("Connection thread") << "Waiting for sum...\n";
+        debug::debug_log("Sender thread") << "Waiting for sum...\n";
 #endif
-        {
+        if (to_send.empty()) {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            queue_condition.wait(lock, [this]{ return !to_send.empty(); });
+            queue_condition.wait(lock, [this]{ return !to_send.empty() || std::cin.eof(); });
+        }
+        if (to_send.empty()) {
+            break;
         }
         int sum = to_send.front();
-        to_send.pop();
 #ifdef DEBUG
-        debug::debug_log("Connection thread") << "Got sum " << sum << "\n";
+        debug::debug_log("Sender thread") << "Got sum " << sum << "\n";
 #endif
-        std::string str = std::to_string(sum);
+        std::string str = std::to_string(sum) + '\n';
 
         accept_new_connections();
 
@@ -93,23 +95,28 @@ void connection::handle_connections() {
 
             polling.erase(std::remove_if(polling.begin(), polling.end(), [](pollfd pfd){ return pfd.fd == -1; }), polling.end());
         }
-        if (!sent) {
-#ifdef DEBUG
-            debug::debug_log("Connection thread") << "Failed to send, repushing " << sum << std::endl;
-#endif
-            to_send.push(sum);
+        if (sent) {
+            to_send.pop();
         }
+#ifdef DEBUG
+        else {
+            debug::debug_log("Sender thread") << "Failed to send, do not pop " << sum << std::endl;
+        }
+#endif
     }
+#ifdef DEBUG
+            debug::debug_log("Sender thread") << "Terminating...\n";
+#endif
 }
 
-void connection::accept_new_connections() {
+void sum_sender::accept_new_connections() {
     int new_fd;
     errno = 0;
     if (fds.empty()) {
         int flags = fcntl(sockfd, F_GETFL);
         fcntl(sockfd, F_SETFL, flags & (~O_NONBLOCK));
 #ifdef DEBUG
-        debug::debug_log("Connection thread") << "Waiting for new connections...\n";
+        debug::debug_log("Sender thread") << "Waiting for new connections...\n";
 #endif
         new_fd = accept(sockfd, NULL, NULL);
         if (new_fd < 0) {
@@ -118,7 +125,7 @@ void connection::accept_new_connections() {
         }
         else {
 #ifdef DEBUG
-            debug::debug_log("Connection thread") << "New connection established " << new_fd << std::endl;
+            debug::debug_log("Sender thread") << "New connection established " << new_fd << std::endl;
 #endif
             fds.insert(new_fd);
         }
@@ -135,21 +142,25 @@ void connection::accept_new_connections() {
             break;
         }
 #ifdef DEBUG
-        debug::debug_log("Connection thread") << "New connection established " << new_fd << std::endl;
+        debug::debug_log("Sender thread") << "New connection established " << new_fd << std::endl;
 #endif
         fds.insert(new_fd);
     } while (new_fd != -1);
 }
 
-void connection::send_sum(int sum) {
+void sum_sender::send_sum(int sum) {
     {
         std::lock_guard<std::mutex> lock(queue_mutex);
         to_send.push(sum);
     }
-    queue_condition.notify_all();
+    queue_condition.notify_one();
 }
 
-connection::~connection() {
+sum_sender::~sum_sender() {
+#ifdef DEBUG
+    debug::debug_log("Sender") << "Terminating connections...\n";
+#endif
+    queue_condition.notify_one();
     for (int fd : fds) {
         close(fd);
     }
